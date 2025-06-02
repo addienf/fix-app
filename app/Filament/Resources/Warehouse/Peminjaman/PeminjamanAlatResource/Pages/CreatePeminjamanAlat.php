06209ee859f6d8fe1cf5a11a275c2dba8108749e
addienaf <addienaf2001@gmail.com>
2025-06-02 15:45:13 +0700

    Peminjaman Alat

---
+++ app/Filament/Resources/Warehouse/Peminjaman/PeminjamanAlatResource/Pages/CreatePeminjamanAlat.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\Warehouse\Peminjaman\PeminjamanAlatResource\Pages;
+
+use App\Filament\Resources\Warehouse\Peminjaman\PeminjamanAlatResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePeminjamanAlat extends CreateRecord
+{
+    protected static string $resource = PeminjamanAlatResource::class;
+    protected static bool $canCreateAnother = false;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
